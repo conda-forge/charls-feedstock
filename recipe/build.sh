e7823fc31fc8457f7f9a410e0173e129b8806290
Mark Harfouche <mark.harfouche@gmail.com>
2020-01-01 10:24:03 -0500

    Update the recipe again for windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,9 @@ cmake -LAH                                                                \
     -DCMAKE_INSTALL_PREFIX=${PREFIX}                                      \
     -DCMAKE_INSTALL_LIBDIR="lib"                                          \
     -DBUILD_SHARED_LIBS=1                                                 \
+    -DCHARLS_BUILD_TESTS=1                                                \
+    -DCHARLS_BUILD_SAMPLES=0                                              \
+    -DCHARLS_INSTALL=1                                                    \
     ..
 
 make -j${CPU_COUNT}
